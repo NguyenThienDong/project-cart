8f143d51d6e771d37f0b8bd8f702ee9c255933e3
DongNguyen <nguyenthiendong17031998@gmail.com>
2021-09-03 02:06:06 +0700

    Use redux

---
+++ src/constants/Message.js
@@ -0,0 +1,5 @@
+export const MSG_ADD_TO_CART_SUCCESS = 'Đặt hàng thành công !';
+export const MSG_UPDATE_CART_SUCCESS = 'Cập nhật giỏ hàng thành công !';
+export const MSG_DELETE_CART_SUCCESS = 'Xóa đơn hàng thành công !';
+export const MSG_CART_EMPTY = 'Giỏ hàng trống !';
+export const MSG_WELLCOME = 'Chào mừng đến với Shopping online !';
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import appReducers from './reducers/index';
+
+const store = createStore(
+    appReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
     document.getElementById('root')
 );
 
